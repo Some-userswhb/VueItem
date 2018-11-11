2681712712981a9feb0c3d46b4afa347efaa85e2
wubinbin <981947064@qq.com>
2018-11-11 16:40:20 +0800

    创建仓库第一次提交

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h2>{{msg}}</h2>
+    <img src="../assets/logo.png" alt="logo">
+  </div>
+</template>
+
+
+<script>
+  export default{
+    data () {
+     return{
+       msg:'Hello World'
+     }
+    }
+  }
+</script>
+
+<style>
+  h2{
+    color: aqua;
+    font-size: 30px;
+  }
+  img{
+    width: 100px;
+    height: 100px;
+  }
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,18 @@
+/**
+ * Created by 98194 on 2018/11/11.
+ */
+import Vue from 'vue'
+
+import App from './App.vue'
+
+ new Vue({
+   el:'#app',
+   components:{
+     App
+   },
+   //解析到html app里面
+   template:'<App/>'
+
+
+ })
+
